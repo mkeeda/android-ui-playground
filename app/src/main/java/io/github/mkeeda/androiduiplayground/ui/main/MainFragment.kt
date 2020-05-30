a2cb7d01b75a4a5eba760f1cab852d97571846b2
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2020-05-30 19:04:50 +0900

    update libraries

--- app/src/main/java/io/github/mkeeda/androiduiplayground/ui/main/MainFragment.kt
+++ app/src/main/java/io/github/mkeeda/androiduiplayground/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package io.github.mkeeda.androiduiplayground.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import io.github.mkeeda.androiduiplayground.R
 
 class MainFragment : Fragment() {
@@ -14,17 +14,10 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
