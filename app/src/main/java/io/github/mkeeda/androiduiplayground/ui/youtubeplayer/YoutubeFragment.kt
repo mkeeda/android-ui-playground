c95742f605df41a556ff9d6a72654251ad23993b
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2020-09-03 23:07:22 +0900

    Add YoutubeFragment

---
+++ app/src/main/java/io/github/mkeeda/androiduiplayground/ui/youtubeplayer/YoutubeFragment.kt
@@ -0,0 +1,12 @@
+package io.github.mkeeda.androiduiplayground.ui.youtubeplayer
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import io.github.mkeeda.androiduiplayground.R
+
+class YoutubeFragment : Fragment(R.layout.fragment_youtube) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
